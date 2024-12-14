fcb4745c3b8ea8a7090b9c83c6faa90cfadfb8a6
mohamedhany606123 <mohamedhany606123@gmail.com>
2024-12-15 00:47:50 +0200

    no message

--- Hany.c
+++ Hany.c
@@ -3,3 +3,24 @@
 #include "Akram.h"
 #include "Amle.h"
 #include "Menna.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int printMainMenu () {
+    int choice;
+    printf("*******************************\n");
+    printf("*      Heliopolise Hotel      *\n");
+    printf("*******************************\n");
+    printf("-the main menu : \n");
+    printf("1. reservation\n");
+    printf("2. check in \n");
+    printf("3. cancel reservatoin \n");
+    printf("4. check room availability \n");
+    printf("5. view customer details\n");
+    printf("6. Query\n");
+    printf("7. reservation report \n");
+    printf("8. Exit \n");
+    printf("please enter your choice (1 - 7) : ");
+    scanf("%d", &choice);
+return choice;
+}
