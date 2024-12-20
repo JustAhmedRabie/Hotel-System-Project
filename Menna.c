a1f19f060d3f79078d4575cff762d65aca35f42f
ahmed_rabie_eg <eizomizo12@gmail.com>
2024-12-20 15:07:38 +0200

    Edited Menna's code.

--- Menna.c
+++ Menna.c
@@ -5,20 +5,20 @@
 #include "Menna.h"
 void LoadRooms(room roomsData[])
 {
-    FILE *roomsfile=fopen("rooms.txt","r");
-    
-    if(roomsfile==NULL)
+    FILE *roomsfile = fopen("rooms.txt", "r");
+
+    if (roomsfile == NULL)
     {
         printf("Error!");
         getche();
         return;
     }
     int i = 0;
-    char x ='a';
-    
-    while(x!= EOF)
+    char x = 'a';
+
+    while (x != EOF)
     {
-        fscanf(roomsfile,"%d %s %s %d",&roomsData[i].roomNumber,roomsData[i].status,roomsData[i].category,&roomsData[i].price);
+        fscanf(roomsfile, "%d %s %s %d", &roomsData[i].roomNumber, roomsData[i].status, roomsData[i].category, &roomsData[i].price);
         x = fgetc(roomsfile);
         i++;
     }
@@ -27,87 +27,98 @@ void LoadRooms(room roomsData[])
     fclose(roomsfile);
 }
 
-int is_vaild_name(const char *name){
-for(int i=0;name[i]!='\0';i++)
+int is_vaild_name(const char *name)
 {
-    if(!isalpha(name[i]) && name[i]!=' '){ 
-        printf("please enter letters only");
-    return 0;}
-}
-return 1;}
-
-int is_vaild_mobil(const char*mobil)
-{
-    for(int i=0;mobil[i]!='\0';i++){ 
-
-    if(!isdigit(mobil[i])){
-        printf("please enter digits only");
-        return 0;}
-    }
-    return 1;
-}
-
-int is_vaild_Date(date date )
-{ 
-    if (date.years!=2024||date.years!=2025)
-    { printf("Error!!");
-        return 0;
-    }
-    //check year of reservation
-
-    if (date.months<1 || date.months>12)
-    {printf("Error!!");
-        return 0;
-    }
-    //check month
-
-    //check no of days dpend on which month
-    int days_in_month[]={31,28,31,30,31,30,31,31,30,31,30,31};
-
-    if(date.years %4==0)
-    { 
-        days_in_month[1]=29;
-    }
-
-    if(date.days<1 || date.days>days_in_month[date.months-1])
-    {printf("Error!!");
-        return 0;}
-    return 1;
-    
-    
-}
-
-int is_vaild_Email(const char* email)
-{ 
-    int at_found=0,dot_found=0;
-    for(int i=0;email[i]!='\0';i++)
-    if (email[i]=='@')
-    { 
-        at_found=1;
-    }
-    if(email[i]=='.'&& at_found)
-    { 
-        dot_found=1;
-    }
-    return dot_found && at_found;
-}
-
-int is_valid_nationalid(const char* nationalid)
-{
-    if(strlen(nationalid)!=14)
-    { printf("please should enter 14 number");
-        return 0;
-    }
-    for("int i=0;i<14;i++")
+    for (int i = 0; name[i] != '\0'; i++)
     {
-        if (nationalid[i]<'0'|| nationalid[i]>'9')
-
+        if (!isalpha(name[i]) && name[i] != ' ')
         {
-            printf("Invalid ID must contain only digit.\n");
+            printf("Please enter letters only\n");
             return 0;
-
         }
     }
     return 1;
 }
 
+int is_vaild_mobil(const char *mobil)
+{
+    for (int i = 0; mobil[i] != '\0'; i++)
+    {
+
+        if (!isdigit(mobil[i]))
+        {
+            printf("Please enter digits only\n");
+            return 0;
+        }
+    }
+    if (strlen(mobil) != 11) return 0;
+    return 1;
+}
+
+int is_vaild_Date(date date)
+{
+    if (date.years != 2024 && date.years != 2025)
+    {
+        printf("ERROR, Date is invalid!!\n");
+        return 0;
+    }
+    // check year of reservation
+
+    if (date.months < 1 || date.months > 12)
+    {
+        printf("ERROR, Date is invalid!!\n");
+        return 0;
+    }
+    // check month
+
+    // check no of days dpend on which month
+    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if (date.years % 4 == 0)
+    {
+        days_in_month[1] = 29;
+    }
+
+    if (date.days < 1 || date.days > days_in_month[date.months - 1])
+    {
+        printf("ERROR, Date is invalid!!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int is_vaild_Email(const char *email)
+{
+    int at_found = 0, dot_found = 0;
+    for (int i = 0; email[i] != '\0'; i++)
+    {
+        if (email[i] == '@')
+        {
+            at_found = 1;
+        }
+        if (email[i] == '.' && at_found)
+        {
+            dot_found = 1;
+        }
+        return dot_found && at_found;
+    }
+}
+
+int is_valid_nationalid(const char *nationalid)
+{
+    if (strlen(nationalid) != 14)
+    {
+        printf("ERROR! national ID must be 14 digits!!\n");
+        return 0;
+    }
+    for (int i = 0; i < 14; i++)
+    {
+        if (nationalid[i] < '0' || nationalid[i] > '9')
+
+        {
+            printf("ID must contain only digits!!\n");
+            return 0;
+        }
+    }
+    return 1;
+}
