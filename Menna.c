10ce5f9f11bbdda65e3418c96fcb0807383008db
mennashi31 <mennashi31@gmail.com>
2024-12-20 13:48:30 +0200

    add validity functions of custumer info.

--- Menna.c
+++ Menna.c
@@ -26,3 +26,88 @@ void LoadRooms(room roomsData[])
 
     fclose(roomsfile);
 }
+
+int is_vaild_name(const char *name){
+for(int i=0;name[i]!='\0';i++)
+{
+    if(!isalpha(name[i]) && name[i]!=' '){ 
+        printf("please enter letters only");
+    return 0;}
+}
+return 1;}
+
+int is_vaild_mobil(const char*mobil)
+{
+    for(int i=0;mobil[i]!='\0';i++){ 
+
+    if(!isdigit(mobil[i])){
+        printf("please enter digits only");
+        return 0;}
+    }
+    return 1;
+}
+
+int is_vaild_Date(date date )
+{ 
+    if (date.years!=2024||date.years!=2025)
+    { printf("Error!!");
+        return 0;
+    }
+    //check year of reservation
+
+    if (date.months<1 || date.months>12)
+    {printf("Error!!");
+        return 0;
+    }
+    //check month
+
+    //check no of days dpend on which month
+    int days_in_month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if(date.years %4==0)
+    { 
+        days_in_month[1]=29;
+    }
+
+    if(date.days<1 || date.days>days_in_month[date.months-1])
+    {printf("Error!!");
+        return 0;}
+    return 1;
+    
+    
+}
+
+int is_vaild_Email(const char* email)
+{ 
+    int at_found=0,dot_found=0;
+    for(int i=0;email[i]!='\0';i++)
+    if (email[i]=='@')
+    { 
+        at_found=1;
+    }
+    if(email[i]=='.'&& at_found)
+    { 
+        dot_found=1;
+    }
+    return dot_found && at_found;
+}
+
+int is_valid_nationalid(const char* nationalid)
+{
+    if(strlen(nationalid)!=14)
+    { printf("please should enter 14 number");
+        return 0;
+    }
+    for("int i=0;i<14;i++")
+    {
+        if (nationalid[i]<'0'|| nationalid[i]>'9')
+
+        {
+            printf("Invalid ID must contain only digit.\n");
+            return 0;
+
+        }
+    }
+    return 1;
+}
+
--- Menna.h
+++ Menna.h
@@ -2,5 +2,10 @@
 #define MENNA_H
 
 void LoadRooms(room roomsData[]);
+int is_vaild_name(const char *name);
+int is_vaild_mobil(const char*mobil);
+int is_vaild_Date(date date );
+int is_vaild_Email(const char* email);
+int is_valid_nationalid(const char* nationalid);
 
 #endif
