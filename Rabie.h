c7f8418bf69679233adb5beecb3d6eaef91416a1
ahmed_rabie_eg <eizomizo12@gmail.com>
2024-12-25 11:24:35 +0200

    Added color macros

--- Rabie.h
+++ Rabie.h
@@ -7,6 +7,12 @@
 #include <conio.h>
 #include "Amle.h"
 
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
 void LogIn();
 void LoadUsers(user usersData[]);
 
